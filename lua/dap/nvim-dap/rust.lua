80844ab9ea82a61fc36820a32f649665b3bf0738
KristyleM <17635675245@163.com>
2022-10-02 22:51:43 +0800

    Fix can not found codelldb bug for rust.

--- lua/dap/nvim-dap/rust.lua
+++ lua/dap/nvim-dap/rust.lua
@@ -1,5 +1,5 @@
 -- 注意：这里要修改为你的绝对路径
-local extension_path = '/Users/heqilong/.local/share/nvim/mason/packages/codelldb/extension'
+local extension_path = '/Users/heqilong/.local/share/nvim/mason/packages/codelldb/extension/'
 local codelldb_path = extension_path .. 'adapter/codelldb'
 local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
 
